721cf34897273f4dc4c7fe97d5496441f283d427
irisida <20445648+irisida@users.noreply.github.com>
2019-02-13 21:58:22 +0000

    added userServices to app and get a basic return of data. token issue requires a refresh

---
+++ src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { Photo } from './photo';
+
+export interface User {
+  id: number;
+  userame: String;
+  knownAs: String;
+  age: number;
+  gender: String;
+  created: Date;
+  lastActive: Date;
+  photoUrl: String;
+  city: String;
+  country: String;
+  interests?: String;
+  introduction?: String;
+  lookingFor?: String;
+  photos: Photo[];
+}
